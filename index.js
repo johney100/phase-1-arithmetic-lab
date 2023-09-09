33f69bf1395fd90edd3bea3c14634695c58f25d5
johney <johneyburke@gmail.com>
2023-09-08 22:13:42 -0600

    math lab 1

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+ let num1 = 6.2;
+ let num2 = 10;
+
+let multiply = num1 * num2 ;
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 8;
+const num4 = 2;
+
+const mod = num3 / num4 ;
+
+const max = Math.max(1, 2, 3, 4, 5, 20);
